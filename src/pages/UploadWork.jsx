659387b2661b29e3d93254eeff7fa12f4bf37994
hesham1221 <hishammohmed12345@gmail.com>
2022-05-28 23:07:22 +0200

    finishing upload work logic and almost done

--- src/pages/UploadWork.jsx
+++ src/pages/UploadWork.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import "../styles/uploadwork.scss";
 import { FiUpload } from "react-icons/fi";
+import {AiFillGithub} from 'react-icons/ai'
+import {BsGlobe} from 'react-icons/bs'
 const UploadWork = ({ isAdmin, setIsAdmin }) => {
+  const serverUrl = 'http://localhost:5000/'
   const [work, setWork] = useState({});
   const [imgs, setImgs] = useState([]);
+  const [backgroundUrl,setBackgroundUrl] = useState('')
+  const [othersUrl,setothersUrl] = useState([''])
   const [imgDetails, setImgDetails] = useState([
     {
       url: "https://designshack.net/wp-content/uploads/placeholder-image.png",
@@ -32,22 +37,83 @@ const UploadWork = ({ isAdmin, setIsAdmin }) => {
   }
   const setAsBackground = (url) => {
     const nImgs = imgs;
-    console.log(nImgs);
-    
     if(nImgs[nImgs.findIndex((imag) => imag[0].background === true)]){
       nImgs[nImgs.findIndex((imag) => imag[0].background === true)][0].background = false;
     }
     nImgs[nImgs.findIndex((imag) => imag[0].url === url)][0].background = true;
-    setImgDetails(nImgs[nImgs.findIndex((imag) => imag[0].url === url)]);
+    const selectedImg = nImgs[nImgs.findIndex((imag) => imag[0].url === url)]
+    setImgDetails(selectedImg);
     setImgs(nImgs);
   };
   const deleteImage =(url)=>{
     const oldImgs = imgs
     const filteredIndex = oldImgs.findIndex(img => img[0].url ===url)
     oldImgs.splice(filteredIndex,1)
-    console.log( filteredIndex , oldImgs)
-    // setImgDetails(imgs[filteredIndex - 1])
+    if(filteredIndex === 0){
+      if(imgs.length === 0){
+        setImgDetails([{
+          url: "https://designshack.net/wp-content/uploads/placeholder-image.png",
+          background: false,
+        },])
+      }else if(imgs.length > 0){
+        setImgDetails(imgs[0])
+      }
+    }else{
+      setImgDetails(imgs[filteredIndex -1])
+    }
     setImgs(oldImgs)
+  }
+
+  const uploadWork = async(e)=>{
+    e.preventDefault()
+    const backgroundImgFile = imgs.filter(img => img[0].background === true)[0][0].file
+    const otherImgs = imgs.filter(img => img[0].background !== true).map(img => img[0].file)
+    const backgroundFormData = new FormData()
+    const otherFormData = new FormData()
+    backgroundFormData.append('photo' , backgroundImgFile)
+    otherImgs.forEach(img => {
+      otherFormData.append('photo' , img)
+    })
+    try {
+      const sentData = await fetch(`${serverUrl}imageupload`,{
+        method :'POST',
+        body:backgroundFormData
+      })
+      const data = await sentData.json()
+      setBackgroundUrl(`${serverUrl}images/${data[0].filename}`)
+      const sentData2 = await fetch(`${serverUrl}imageupload`,{
+        method :'POST',
+        body:otherFormData
+      })
+      const data2 = await sentData2.json()
+      console.log(data2)
+      const otherUrl = data2.map(other => `${serverUrl}images/${other.filename}`)
+      setothersUrl(otherUrl)
+      const upWork = {
+        workTitle : work.title,
+        workPhotos :{
+          background: backgroundUrl,
+          otherPhotos :othersUrl
+        },
+        links :{
+          github : work.github,
+          live :work.live
+        },
+        workDescribtion : work.details
+      }
+      const sendWork = await fetch(`${serverUrl}newwork`,{
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(upWork)
+      })
+      const finalData = await sendWork.json()
+      console.log(finalData)
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
   return (
     <div className="uploadWork">
@@ -74,6 +140,30 @@ const UploadWork = ({ isAdmin, setIsAdmin }) => {
                 setWork((work) => ({ ...work, details: tar.target.value }))
               }
             />
+            <div style={{display : 'flex' , alignItems :'center'}}>
+
+            <AiFillGithub style={{fontSize : '30px' , marginRight : '6px'}} /> <input
+              className={`form_input`}
+              value={work.github}
+              placeholder="Github Link..."
+              style={{width : '63.5%'}}
+              onChange={(tar) =>
+                setWork((work) => ({ ...work, github : tar.target.value }))
+              }
+            />
+            </div>
+            <div style={{display : 'flex',marginTop : '9px' , alignItems :'center'}}>
+
+            <BsGlobe style={{fontSize : '30px' , marginRight : '6px'}} /> <input
+              className={`form_input`}
+              value={work.live}
+              placeholder="Live Link..."
+              style={{width : '63.5%'}}
+              onChange={(tar) =>
+                setWork((work) => ({ ...work, live: tar.target.value }))
+              }
+            />
+            </div>
             <h2>Images :</h2>
             <label className="uploadLabel" htmlFor="upload">
               <FiUpload className="uploadIcon" />
@@ -98,7 +188,7 @@ const UploadWork = ({ isAdmin, setIsAdmin }) => {
                 />
               ))}
             </div>
-            <button type="submit" className={`sendBtn`}>
+            <button onClick={uploadWork} type="submit" className={`sendBtn`}>
               Upload
             </button>
           </form>
